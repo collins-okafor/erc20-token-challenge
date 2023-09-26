a3f9506c6551dc7f66db6fd9efd86c2bd0b56523
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-09-26 05:22:00 +0100

    key setups added

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,5 +2,21 @@
 
 pragma solidity ^0.8.16;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20, AccessControl {
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(uint256 initialSupply) ERC20("Sonet Token", "CIO"){
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE){
+        _mint(to, amount);
+    }
+}
 
